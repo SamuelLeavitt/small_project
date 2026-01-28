316349fa59d4e2a77f1279ac5dc77f4174603a19
root <sa477571@ucf.edu>
2026-01-28 06:21:04 +0000

    Started writing a script to create the database

---
+++ database_scripts/schema.sql
@@ -0,0 +1,26 @@
+-- Database creation script
+
+CREATE DATABASE IF NOT EXISTS contact_manager;
+USE contact_manager;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(254) NOT NULL,
+  password VARCHAR(50) NOT NULL, -- Need to validate characters needed for password
+  PRIMARY KEY (id),
+  UNIQUE (username),
+  UNIQUE (email)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS contacts (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL,
+  email VARCHAR(254) NOT NULL,
+  creation_date VARCHAR(8),
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;
