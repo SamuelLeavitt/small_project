e8e565262cf462b3819aac96faf5d2ff2e5e9540
root <sa477571@ucf.edu>
2026-01-28 07:30:08 +0000

    Increased storage space for passwords to facilitate argon2 hashed passwords

--- database_scripts/schema.sql
+++ database_scripts/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(50) NOT NULL,
   email VARCHAR(254) NOT NULL,
-  password VARCHAR(50) NOT NULL, -- Need to validate characters needed for password
+  password_hash VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (username),
   UNIQUE (email)
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS contacts (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  phone_number VARCHAR(15) NOT NULL,
+  phone_number VARCHAR(30) NOT NULL,
   email VARCHAR(254) NOT NULL,
   creation_date DATE DEFAULT (CURRENT_DATE()),
   user_id INT NOT NULL,
