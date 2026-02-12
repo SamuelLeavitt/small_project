c299a02d5ce53022880421b9e9e8e6e5c0fe9a54
Jose Serrano <185645269+jasc1204@users.noreply.github.com>
2026-02-12 07:11:23 +0000

    fixed comment about myqli

--- backend/connection.php
+++ backend/connection.php
@@ -1,6 +1,6 @@
 <?php
+
 // so we don't need to add more files for connection details
 // Make sure to copy the same database from schema.sql into digital ocean mysql
-// Add your own ip and credentials here
-$conn = new mysqli("YourDigitalOceanIP", "root", "YOUR_PASSWORD", "contact_manager");
-?>
+//  create a new user with password and give it permissions to that database, then update the connection details below.
+$conn = new mysqli("localhost", "appuser", "SomeStrongPassword1!", "contact_manager");
