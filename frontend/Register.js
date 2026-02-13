949eb2d35613dced9ba5233d77323fe5592dce27
Renard Ali <renardtnt1@gmail.com>
2026-02-13 12:52:46 -0500

    Starting to create the front end for the register

---
+++ frontend/Register.js
@@ -0,0 +1,7 @@
+// Register shape: { username, email, password}
+const urlBase = "backend/routes/Auth";
+const extension = "php";
+
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
