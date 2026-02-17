8b2beea4b2b4b3207ff8aec4ae39f9b27700b2ed
Jose Serrano <185645269+jasc1204@users.noreply.github.com>
2026-02-17 07:30:44 +0000

    Deleted repeated files with no content within them

--- frontend/Register.js
+++
@@ -1,9 +0,0 @@
-// Register shape: { username, email, password}
-const urlBase = "backend/routes/Auth";
-const extension = "php";
-
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-
-
